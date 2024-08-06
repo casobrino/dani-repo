ea08d5162aa0de68f97b80f44e4bcec940dd5550
cesar sobrino <casobrino_0@hotmail.com>
2024-08-05 19:20:02 -0600

    dani

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from 'cors'
 import path from "path";
 import { fileURLToPath } from "url";
+
 
 // Definir __dirname manualmente
 const __filename = fileURLToPath(import.meta.url);
@@ -8,15 +10,18 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(cors())
+
 app.get("/", (req, res) => {
   res.sendFile("views/index.html", { root: __dirname });
 });
 
 app.get("/api/data", async (req, res) => {
+  console.log(req)
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    const data = await response.json();
-    res.json(data);
+    const response = await fetch('https://fakestoreapi.com/products/category/jewelery');
+    const result = await response.json();
+    res.send(result)
   } catch (error) {
     console.error("Error al consumir la API externa:", error);
     res.status(500).json({ error: "Error al consumir la API externa" });
