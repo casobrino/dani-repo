6241c038e06832fa767ac40c8c138b608b53ac12
cesar sobrino <casobrino_0@hotmail.com>
2024-08-05 19:48:18 -0600

    asd

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-import express from "express";
+import express from "express"
 import cors from 'cors'
-import path from "path";
-import { fileURLToPath } from "url";
+import path from "path"
+import { fileURLToPath } from "url"
 
 
 // Definir __dirname manualmente
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const app = express();
+const app = express()
 
 app.use(cors())
 
 app.get("/", (req, res) => {
-  res.sendFile("views/index.html", { root: __dirname });
-});
+  res.sendFile("views/index.html", { root: __dirname })
+})
 
 app.get("/api/data", async (req, res) => {
-  console.log(req)
+  const query = req.query.query // Recuperar el parámetro 'query' de la URL
+  console.log(`Query recibido: ${query}`)
+
   try {
-    const response = await fetch('https://fakestoreapi.com/products/category/jewelery');
-    const result = await response.json();
-    res.send(result)
+    if (query == null || query == "") {
+      const response = await fetch('https://fakestoreapi.com/products/categories')
+      const result = await response.json()
+      res.send(result)
+    }
+    else {
+      const response = await fetch(`https://fakestoreapi.com/products/category/${query}`)
+      const result = await response.json()
+      res.send(result)
+    }
+
   } catch (error) {
-    console.error("Error al consumir la API externa:", error);
-    res.status(500).json({ error: "Error al consumir la API externa" });
+    console.error("Error al consumir la API externa:", error)
+    res.status(500).json({ error: "Error al consumir la API externa" })
   }
-});
+})
 
 
 app.listen(3000, function () {
-  console.log("escuchando el puerto 3000!");
-});
+  console.log("escuchando el puerto 3000!")
+})
