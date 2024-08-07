73ce89a8ecbbdcd7027241549e98d8d17c5aa2d7
Cesar Sobrino <cesar.sobrino.billboardplanet@gmail.com>
2024-08-07 09:17:11 -0600

    conexion agregada

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from 'cors'
 import path from "path"
 import { fileURLToPath } from "url"
-import sqlite3 from 'sqlite3';
+import sqlite3 from 'sqlite3'
 
 
 // Definir __dirname manualmente
@@ -19,37 +19,35 @@ app.get("/", (req, res) => {
 
 const db = new sqlite3.Database('./mi_base_de_datos.db', (err) => {
   if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-    return;
+    console.error('Error al conectar a la base de datos:', err)
+    return
   }
-  console.log('Conectado a la base de datos SQLite');
-});
+  console.log('Conectado a la base de datos SQLite')
+})
 
-app.get('/datos', (req, res) => {
-  const sql = 'SELECT * FROM mi_tabla';
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      res.status(500).send('Error al obtener datos');
-      return;
-    }
-    res.json(rows);
-  });
-});
 
 app.get("/api/data", async (req, res) => {
   const query = req.query.query
 
   try {
-    if (query == null || query == "") {
-      const response = await fetch('https://fakestoreapi.com/products/categories')
-      const result = await response.json()
-      res.send(result)
-    }
-    else {
-      const response = await fetch(`https://fakestoreapi.com/products/category/${query}`)
-      const result = await response.json()
-      res.send(result)
-    }
+    const sql = `SELECT * FROM mi_tabla WHERE palabra LIKE %${query}`
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        res.status(500).send('Error al obtener datos')
+        return
+      }
+      res.json(rows)
+    })
+    //   if (query == null || query == "") {
+    //     const response = await fetch('https://fakestoreapi.com/products/categories')
+    //     const result = await response.json()
+    //     res.send(result)
+    //   }
+    //   else {
+    //     const response = await fetch(`https://fakestoreapi.com/products/category/${query}`)
+    //     const result = await response.json()
+    //     res.send(result)
+    //   }
 
   } catch (error) {
     console.error("Error al consumir la API externa:", error)
